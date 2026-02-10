d610e6b1b415531cba0582d1bd73cc31e48a886d
Juan Sebastian <jsebaskno@gmail.com>
2026-02-10 09:04:06 -0600

    init_commit

---
+++ Lesson1/src/main/java/edu/example/lesson1/MyClass.kt
@@ -0,0 +1,4 @@
+package edu.example.lesson1
+
+class MyClass {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kotlin_Conceptos_Basicos"
+include(":app")
+include(":Lesson1")
